d8016c5f49c86444cd606abe85eef0b153c84979
Wesley Stagg <wes.stagg@gmail.com>
2026-04-10 13:40:13 +0100

    added tests popBestOrderFromDuplicatePriceOrders and getBestPriceAfterOrderRemoved

--- tests/sortedOrderListTests.cpp
+++ tests/sortedOrderListTests.cpp
@@ -103,6 +103,12 @@ TEST(SortedOrderListTests, removeOrderFail)
     EXPECT_EQ(askOrderbook.getSize(), 2); 
 }
 
+TEST(SortedOrderListTests, removeOrderFromEmptyOrderList)
+{
+    obe::SortedOrderList<obe::AskComparator> askOrderbook;
+    EXPECT_EQ(askOrderbook.removeOrder(1), false);
+}
+
 TEST(SortedOrderListTests, popBestAskOrder)
 {
     obe::SortedOrderList<obe::AskComparator> askOrderbook;
@@ -147,4 +153,47 @@ TEST(SortedOrderListTests, popBestOrderOnEmptyOrderList)
     auto order = askOrderbook.popBestOrder();
 
     EXPECT_EQ(order, std::nullopt);
+}
+
+TEST(SortedOrderListTests, popBestOrderFromDuplicatePriceOrders)
+{
+    obe::SortedOrderList<obe::BidComparator> bidOrderbook;
+    obe::Price price = obe::Price::fromPence(100);
+
+    obe::Order order1 {1, price, 1};
+    obe::Order order2 {2, price, 2};
+    bidOrderbook.insert(order1);
+    bidOrderbook.insert(order2);
+    
+    auto orderOne = bidOrderbook.popBestOrder();
+
+    EXPECT_EQ(orderOne.value().id, 1);
+    EXPECT_EQ(orderOne.value().price.pence, price.pence);
+    EXPECT_EQ(orderOne.value().quantity, 1);
+    EXPECT_EQ(bidOrderbook.getSize(), 1); 
+
+    auto orderTwo = bidOrderbook.popBestOrder();
+
+    EXPECT_EQ(orderTwo.value().id, 2);
+    EXPECT_EQ(orderTwo.value().price.pence, price.pence);
+    EXPECT_EQ(orderTwo.value().quantity, 2);
+    EXPECT_EQ(bidOrderbook.getSize(), 0); 
+}
+
+TEST(SortedOrderListTests, getBestPriceAfterOrderRemoved)
+{
+    obe::SortedOrderList<obe::BidComparator> bidOrderbook;
+    obe::Price priceOne = obe::Price::fromPence(100);
+    obe::Price priceTwo = obe::Price::fromPence(99);
+
+    obe::Order order1 {1, priceOne, 1};
+    obe::Order order2 {2, priceTwo, 2};
+    bidOrderbook.insert(order1);
+    bidOrderbook.insert(order2);
+    
+    EXPECT_EQ(bidOrderbook.getBestPrice(), priceOne.pence);
+
+    ASSERT_TRUE(bidOrderbook.removeOrder(1));
+    
+    EXPECT_EQ(bidOrderbook.getBestPrice(), priceTwo.pence);
 }
