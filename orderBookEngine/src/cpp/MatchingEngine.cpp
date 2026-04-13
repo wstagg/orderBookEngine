ec2480559ea83d85949f239d9286b71f92f4ea9e
Wesley Stagg <wes.stagg@gmail.com>
2026-04-13 13:35:14 +0100

    MatchingEngine nuilt with tests

--- orderBookEngine/src/cpp/MatchingEngine.cpp
+++ orderBookEngine/src/cpp/MatchingEngine.cpp
@@ -9,6 +9,7 @@ std::vector<obe::TradeEvent> obe::MatchingEngine::submitOrder(const obe::OrderTy
         break;
     case OrderType::bid:
         bidsList.insert(order);
+        break;
     default:
         break;
     }
@@ -23,7 +24,7 @@ std::vector<obe::TradeEvent> obe::MatchingEngine::matchingLoop()
     // make sure both lists are not empty
     if (asksList.size() > 0 && bidsList.size() > 0)
     {
-        while(asksList.peekBestOrder().value().price.pence <= bidsList.peekBestOrder().value().price.pence)
+        while (asksList.peekBestOrder().value().price.pence <= bidsList.peekBestOrder().value().price.pence)
         {
             if (asksList.peekBestOrder().value().quantity == bidsList.peekBestOrder().value().quantity)
             {
@@ -33,15 +34,30 @@ std::vector<obe::TradeEvent> obe::MatchingEngine::matchingLoop()
                 tradeEvents.emplace_back(bid.value().id, ask.value().id, ask.value().price, ask.value().quantity);
             }
 
-            // check to see if either list has become empty and break out if true
+            else if (asksList.peekBestOrder().value().quantity > bidsList.peekBestOrder().value().quantity)
+            {
+                auto bid = bidsList.popBestOrder();
+                
+                tradeEvents.emplace_back(bid.value().id, asksList.peekBestOrder().value().id, asksList.peekBestOrder().value().price, bid.value().quantity);
+                
+                asksList.reduceOrderQuantity(asksList.peekBestOrder().value().id, (asksList.peekBestOrder().value().quantity - bid.value().quantity));
+            }
+
+            else if (asksList.peekBestOrder().value().quantity < bidsList.peekBestOrder().value().quantity)
+            {
+                auto ask = asksList.popBestOrder();
+                
+                tradeEvents.emplace_back(bidsList.peekBestOrder().value().id, ask.value().id, ask.value().price, ask.value().quantity);
+                
+                bidsList.reduceOrderQuantity(bidsList.peekBestOrder().value().id, (bidsList.peekBestOrder().value().quantity - ask.value().quantity));
+            }
+
+            // check to see if either list has become empty and break out of loop if true
             if (asksList.size() == 0 || bidsList.size() == 0)
             {
                 break;
             }
-        }
-        
-        
-        
+        }   
     }
     return tradeEvents;
 }
--- tests/MatchingEngineTests.cpp
+++ tests/MatchingEngineTests.cpp
@@ -23,6 +23,22 @@ TEST(MatchingEngineTests, submitSingleBid)
     EXPECT_TRUE(trades.empty());
 }
 
+TEST(MatchingEngineTests, bidPriceLessThanAsk)
+{
+    obe::MatchingEngine engine;
+
+    obe::Order bid{1, obe::Price::fromPounds(0.99), 100};
+    obe::Order ask{2, obe::Price::fromPounds(1.00), 100};
+    
+    auto trades1 = engine.submitOrder(obe::OrderType::bid, bid);
+
+    EXPECT_TRUE(trades1.empty());
+
+    auto trades2 = engine.submitOrder(obe::OrderType::ask, ask);
+
+    EXPECT_TRUE(trades2.empty());
+}
+
 TEST(MatchingEngineTests, bidAndAskSamePriceSameQuatity)
 {
     obe::MatchingEngine engine;
@@ -64,3 +80,176 @@ TEST(MatchingEngineTests, askPriceLessQuantityEqual)
     EXPECT_EQ(trades2.front().quantity, 100);
     EXPECT_EQ(trades2.front().price.pence, 99);
 }
+
+TEST(MatchingEngineTests, askQuantityGreaterThanBidQuantityPartialOrderFill)
+{
+    obe::MatchingEngine engine;
+
+    obe::Order bid{1, obe::Price::fromPounds(1.00), 100};
+    obe::Order ask{2, obe::Price::fromPounds(1.00), 200};
+    
+    engine.submitOrder(obe::OrderType::bid, bid);
+    auto trades = engine.submitOrder(obe::OrderType::ask, ask);
+
+    EXPECT_EQ(trades.size(), 1);
+
+    EXPECT_EQ(trades.front().askId, 2);
+    EXPECT_EQ(trades.front().bidId, 1);
+    EXPECT_EQ(trades.front().quantity, 100);
+    EXPECT_EQ(trades.front().price.pence, 100);
+}
+
+TEST(MatchingEngineTests, askQuantityGreaterThanBidQuantityFullOrderFill)
+{
+    obe::MatchingEngine engine;
+
+    obe::Order bid1{1, obe::Price::fromPounds(1.00), 100};
+    obe::Order bid2{2, obe::Price::fromPounds(1.00), 100};
+    obe::Order ask{1, obe::Price::fromPounds(1.00), 200};
+    
+    engine.submitOrder(obe::OrderType::bid, bid1);
+    engine.submitOrder(obe::OrderType::bid, bid2);
+    auto trades = engine.submitOrder(obe::OrderType::ask, ask);
+
+    EXPECT_EQ(trades.size(), 2);
+
+    EXPECT_EQ(trades[0].askId, 1);
+    EXPECT_EQ(trades[0].bidId, 1);
+    EXPECT_EQ(trades[0].quantity, 100);
+    EXPECT_EQ(trades[0].price.pence, 100);
+
+    EXPECT_EQ(trades[1].askId, 1);
+    EXPECT_EQ(trades[1].bidId, 2);
+    EXPECT_EQ(trades[1].quantity, 100);
+    EXPECT_EQ(trades[1].price.pence, 100);
+}
+
+TEST(MatchingEngineTests, bidQuantityGreaterThanAskQuantityPartialOrderFill)
+{
+    obe::MatchingEngine engine;
+
+    obe::Order bid{1, obe::Price::fromPounds(1.00), 200};
+    obe::Order ask{2, obe::Price::fromPounds(1.00), 100};
+    
+    engine.submitOrder(obe::OrderType::bid, bid);
+    auto trades = engine.submitOrder(obe::OrderType::ask, ask);
+
+    EXPECT_EQ(trades.size(), 1);
+
+    EXPECT_EQ(trades.front().askId, 2);
+    EXPECT_EQ(trades.front().bidId, 1);
+    EXPECT_EQ(trades.front().quantity, 100);
+    EXPECT_EQ(trades.front().price.pence, 100);
+}
+
+TEST(MatchingEngineTests, bidQuantityGreaterThanAskQuantityFullOrderFill)
+{
+    obe::MatchingEngine engine;
+
+    obe::Order bid1{1, obe::Price::fromPounds(1.00), 200};
+    obe::Order ask2{2, obe::Price::fromPounds(1.00), 100};
+    obe::Order ask1{1, obe::Price::fromPounds(1.00), 100};
+    
+    engine.submitOrder(obe::OrderType::ask, ask1);
+    engine.submitOrder(obe::OrderType::ask, ask2);
+    auto trades = engine.submitOrder(obe::OrderType::bid, bid1);
+
+    EXPECT_EQ(trades.size(), 2);
+
+    EXPECT_EQ(trades[0].askId, 1);
+    EXPECT_EQ(trades[0].bidId, 1);
+    EXPECT_EQ(trades[0].quantity, 100);
+    EXPECT_EQ(trades[0].price.pence, 100);
+
+    EXPECT_EQ(trades[1].askId, 2);
+    EXPECT_EQ(trades[1].bidId, 1);
+    EXPECT_EQ(trades[1].quantity, 100);
+    EXPECT_EQ(trades[1].price.pence, 100);
+}
+
+TEST(MatchingEngineTests, askQuantityGreaterThanBidQuantityPartialOrderFillBidGreaterPrice)
+{
+    obe::MatchingEngine engine;
+
+    obe::Order bid{1, obe::Price::fromPounds(2.00), 100};
+    obe::Order ask{2, obe::Price::fromPounds(1.00), 200};
+    
+    engine.submitOrder(obe::OrderType::bid, bid);
+    auto trades = engine.submitOrder(obe::OrderType::ask, ask);
+
+    EXPECT_EQ(trades.size(), 1);
+
+    EXPECT_EQ(trades.front().askId, 2);
+    EXPECT_EQ(trades.front().bidId, 1);
+    EXPECT_EQ(trades.front().quantity, 100);
+    EXPECT_EQ(trades.front().price.pence, 100);
+}
+
+TEST(MatchingEngineTests, askQuantityGreaterThanBidQuantityFullOrderFillBidGreaterPrice)
+{
+    obe::MatchingEngine engine;
+
+    obe::Order bid1{1, obe::Price::fromPounds(2.00), 100};
+    obe::Order bid2{2, obe::Price::fromPounds(2.00), 100};
+    obe::Order ask{1, obe::Price::fromPounds(1.00), 200};
+    
+    engine.submitOrder(obe::OrderType::bid, bid1);
+    engine.submitOrder(obe::OrderType::bid, bid2);
+    auto trades = engine.submitOrder(obe::OrderType::ask, ask);
+
+    EXPECT_EQ(trades.size(), 2);
+
+    EXPECT_EQ(trades[0].askId, 1);
+    EXPECT_EQ(trades[0].bidId, 1);
+    EXPECT_EQ(trades[0].quantity, 100);
+    EXPECT_EQ(trades[0].price.pence, 100);
+
+    EXPECT_EQ(trades[1].askId, 1);
+    EXPECT_EQ(trades[1].bidId, 2);
+    EXPECT_EQ(trades[1].quantity, 100);
+    EXPECT_EQ(trades[1].price.pence, 100);
+}
+
+TEST(MatchingEngineTests, bidQuantityGreaterThanAskQuantityPartialOrderFillBidGreaterPrice)
+{
+    obe::MatchingEngine engine;
+
+    obe::Order bid{1, obe::Price::fromPounds(2.00), 200};
+    obe::Order ask{2, obe::Price::fromPounds(1.00), 100};
+    
+    engine.submitOrder(obe::OrderType::bid, bid);
+    auto trades = engine.submitOrder(obe::OrderType::ask, ask);
+
+    EXPECT_EQ(trades.size(), 1);
+
+    EXPECT_EQ(trades.front().askId, 2);
+    EXPECT_EQ(trades.front().bidId, 1);
+    EXPECT_EQ(trades.front().quantity, 100);
+    EXPECT_EQ(trades.front().price.pence, 100);
+}
+
+TEST(MatchingEngineTests, bidQuantityGreaterThanAskQuantityFullOrderFillBidGreaterPrice)
+{
+    obe::MatchingEngine engine;
+
+    obe::Order bid1{1, obe::Price::fromPounds(2.00), 200};
+    obe::Order ask2{2, obe::Price::fromPounds(1.00), 100};
+    obe::Order ask1{1, obe::Price::fromPounds(1.00), 100};
+    
+    engine.submitOrder(obe::OrderType::ask, ask1);
+    engine.submitOrder(obe::OrderType::ask, ask2);
+    auto trades = engine.submitOrder(obe::OrderType::bid, bid1);
+
+    EXPECT_EQ(trades.size(), 2);
+
+    EXPECT_EQ(trades[0].askId, 1);
+    EXPECT_EQ(trades[0].bidId, 1);
+    EXPECT_EQ(trades[0].quantity, 100);
+    EXPECT_EQ(trades[0].price.pence, 100);
+
+    EXPECT_EQ(trades[1].askId, 2);
+    EXPECT_EQ(trades[1].bidId, 1);
+    EXPECT_EQ(trades[1].quantity, 100);
+    EXPECT_EQ(trades[1].price.pence, 100);
+}
+
